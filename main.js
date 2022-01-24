9199369ae1844faa49e6bfcd79d52883cec2dd80
playedketchup81 <89692389+playedketcup81@users.noreply.github.com>
2022-01-24 15:20:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 var a = Math.floor(Math.random() * 100);{
 
 console.log(a)
-  
+// a is random
 
 
 var b = Math.floor(Math.random() * 100);{
 
 console.log(b)
-
+// b is random
 console.log(a + b)
+ // a + b = c
   
  if (a < b){
  console.log("b is greater than a")
- 
 }else{
   console.log("a is greater than b")
+  // this detects if its greater or less
 }
 
 }}
