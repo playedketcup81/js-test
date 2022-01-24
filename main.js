ce61286a247f2cd901cbdf9cdbce17d5d247886e
playedketchup81 <89692389+playedketcup81@users.noreply.github.com>
2022-01-24 15:24:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ console.log(a)
 // a is random
 
 
+console.log("+")
 var b = Math.floor(Math.random() * 100);{
 
 console.log(b)
-// b is random
+//b is random
+console.log("=")
+
 console.log(a + b)
- // a + b = c
-  
- if (a < b){
+// a + b = c its all random so like a could be 34 on the first try and 59 on the second, the same for b
+if (a < b){
  console.log("b is greater than a")
+ 
 }else{
   console.log("a is greater than b")
-  // this detects if its greater or less
+//this detects if its greater or less than
 }
 
 }}
-
